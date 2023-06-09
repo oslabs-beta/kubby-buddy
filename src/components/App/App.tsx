3e221186855e3f82cbbbcd27a1d1b2288cb8cacc
Kasualkid12 <kasu@kasu-it.dev>
2023-06-09 16:30:15 -0500

    initial commit

---
+++ src/components/App/App.tsx
@@ -0,0 +1,11 @@
+import React, { FC } from 'react';
+
+const App: FC = () => {
+  return (
+    <div>
+      <h1>howdy kubby buddies! YOOO</h1>
+    </div>
+  )
+};
+
+export default App;
---
+++ webpack.config.cjs
@@ -0,0 +1,51 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.tsx',
+  devServer: {
+    proxy: {
+      // '/api/**': 'http://localhost:3000',
+    },
+    historyApiFallback: true,
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js', //filler
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      // {
+      //   test: /\.tsx?/, //applys rules to js or jsx
+      //   exclude: /node_modules/,
+      //   use: {
+      //     loader: 'babel-loader',
+      //     options: {
+      //       presets: ['@babel/preset-env', '@babel/preset-react'],
+      //     },
+      //   },
+      // },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+};
