393f42c4eecc80ec8e3b1407dacb8adf59c79339
Steve Lemlek <stevelemlek@gmail.com>
2023-06-10 11:09:10 -0500

    getAllImages via path image/all-images.  updated imageRouter and imageController for this

---
+++ server/controllers/imageController.ts
@@ -0,0 +1,28 @@
+import { Request, Response, NextFunction } from "express"
+import { exec } from "node:child_process"
+
+const imageController = {
+    getAllImages: async (req: Request, res: Response, next: NextFunction) => {
+        
+        try {
+            await exec('docker images', (error, stdout, stderr) => {
+                if (error) {
+                    next({
+                        log: 'error in exec of imageController.getAllImages',
+                        err: error
+                    })
+                }
+                res.locals.images = stdout
+                next()
+            })
+        } catch (error) {
+            next({
+                log: 'error in imageController.getAllImages',
+                err: error
+            })
+        }
+        
+    }
+}
+
+export default imageController
