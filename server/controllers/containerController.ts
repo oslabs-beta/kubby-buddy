5c22fccc38a67fe5215ec77d1e2155423af56334
Steve Lemlek <stevelemlek@gmail.com>
2023-06-10 15:40:29 -0500

    prettier didn't format correctly, manually formatted everything

--- server/controllers/containerController.ts
+++ server/controllers/containerController.ts
@@ -1,28 +1,30 @@
-import { Request, Response, NextFunction } from 'express'
-import { exec } from 'node:child_process'
+import { Request, Response, NextFunction } from 'express';
+import { exec } from 'node:child_process';
 
 const containerController = {
-    getAllRunningContainers: async (_req: Request, res: Response, next: NextFunction) => {
-        try {
-            await exec('docker ps', (error, stdout, _stderr) => {
-                if (error) {
-                    next({
-                        log: 'error in the exec call in containerController.getAllRunningContainers',
-                        err: error
-                    })
-                }
-                res.locals.containers = stdout
-                next()
-            })
+	getAllRunningContainers: async (
+		_req: Request,
+		res: Response,
+		next: NextFunction
+	) => {
+		try {
+			await exec('docker ps', (error, stdout, _stderr) => {
+				if (error) {
+					next({
+						log: 'error in the exec call in containerController.getAllRunningContainers',
+						err: error,
+					});
+				}
+				res.locals.containers = stdout;
+				next();
+			});
+		} catch (error) {
+			next({
+				log: 'error in the containerController.getAllRunningContainers',
+				err: error,
+			});
+		}
+	},
+};
 
-        }
-        catch (error) {
-            next({
-                log: 'error in the containerController.getAllRunningContainers',
-                err: error
-            })
-        }
-    }
-}
-
-export default containerController
+export default containerController;
--- server/routes/containerRoutes.ts
+++ server/routes/containerRoutes.ts
@@ -1,29 +1,26 @@
-import express, { Request, Response } from 'express'
-import containerController from '../controllers/containerController'
+import express, { Request, Response } from 'express';
+import containerController from '../controllers/containerController';
 
-const containerRouter = express.Router()
+const containerRouter = express.Router();
 
-
-
-
-containerRouter.get('/all-active-containers', containerController.getAllRunningContainers, (_req: Request, res: Response) => {
-    res.status(200).json(res.locals.containers)
-})
+containerRouter.get(
+	'/all-active-containers',
+	containerController.getAllRunningContainers,
+	(_req: Request, res: Response) => {
+		res.status(200).json(res.locals.containers);
+	}
+);
 
 containerRouter.use('/start', (_req: Request, res: Response) => {
-    res.send('start test worked')
-})
+	res.send('start test worked');
+});
 
 containerRouter.use('/stop', (_req: Request, res: Response) => {
-    res.send('stop test worked')
-})
+	res.send('stop test worked');
+});
 
 containerRouter.use('/', (_req: Request, res: Response) => {
-    res.send('containerRouter test')
-})
+	res.send('containerRouter test');
+});
 
-
-
-
-    
-export default containerRouter
+export default containerRouter;
