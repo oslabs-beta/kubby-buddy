8abd7052b6fb2d99e72210c452db67ed19a82b52
Steve Lemlek <stevelemlek@gmail.com>
2023-06-10 11:23:40 -0500

    allActiveContainers route added.  update containerRoutes and container Controller

--- server/routes/containerRoutes.ts
+++ server/routes/containerRoutes.ts
@@ -1,7 +1,14 @@
 import express, { Request, Response } from 'express'
+import containerController from '../controllers/containerController'
+
 const containerRouter = express.Router()
 
 
+
+
+containerRouter.get('/all-active-containers', containerController.getAllRunningContainers, (_req: Request, res: Response) => {
+    res.status(200).json(res.locals.containers)
+})
 
 containerRouter.use('/start', (_req: Request, res: Response) => {
     res.send('start test worked')
