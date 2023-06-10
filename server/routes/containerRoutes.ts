e862b28d5658c87dce5403ff466c92fb2d2342a5
Steve Lemlek <stevelemlek@gmail.com>
2023-06-10 09:32:23 -0500

    start and stop route templates added to ontainerRoutes

--- server/routes/containerRoutes.ts
+++ server/routes/containerRoutes.ts
@@ -1,8 +1,22 @@
 import express, { Request, Response } from 'express'
 const containerRouter = express.Router()
 
+
+
+containerRouter.use('/start', (_req: Request, res: Response) => {
+    res.send('start test worked')
+})
+
+containerRouter.use('/stop', (_req: Request, res: Response) => {
+    res.send('stop test worked')
+})
+
 containerRouter.use('/', (_req: Request, res: Response) => {
     res.send('containerRouter test')
 })
 
+
+
+
+    
 export default containerRouter
