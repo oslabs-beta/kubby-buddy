be2ab6886b456d73db5715757da5da1eb4571430
Steve Lemlek <stevelemlek@gmail.com>
2023-06-10 12:32:07 -0500

    volume routes added, commented out the deleteAllVolumes middleware

--- server/controllers/volumeController.ts
+++ server/controllers/volumeController.ts
@@ -22,6 +22,50 @@ const volumeController = {
                 message: error
             })
         }
+    },
+    
+    //there's a work around for mac, but not sure about windows
+    deleteAllVolumes: async (_req: Request, _res: Response, next: NextFunction) => {
+        try {   
+            // await exec('docker volume prune', (error, _stdout, _stderr) => {
+            //     if (error) {
+            //         next({
+            //             log: 'error in the volumeController.deleteAll Volumes exec call',
+            //             message: error
+            //         })
+            //     }
+            //     if (_stdout) {
+            //         try {
+            //             exec('y', (error, stdout, _stderr) => {
+            //                 if (error) {
+            //                     next({
+            //                         log: 'error in exec, in exec of deleteAllVolumes',
+            //                         err: error
+            //                     })
+            //                 }
+            //                 res.locals.deleted = stdout
+            //                 next()
+            //             })
+            //          }
+            //         catch (error) {
+            //             next({
+            //                 log: 'error in second try block in deleteAllVolumes',
+            //                 err: error
+            //             })
+    
+            //         }
+
+            //     }
+
+            // })
+
+        }
+        catch (error) {
+            next({
+                log: 'error in the volumeController.deleteAllVolumes middleware',
+                message: error
+            })
+        }
     }
 }
 
--- server/routes/volumeRouter.ts
+++ server/routes/volumeRouter.ts
@@ -3,8 +3,11 @@ import volumeController from "../controllers/volumeController"
 
 const volumeRouter = express.Router()
 
-volumeRouter.use('/', volumeController.getAllVolumes, (_req: Request, res: Response) => {
+volumeRouter.get('/all-volumes', volumeController.getAllVolumes, (_req: Request, res: Response) => {
     res.status(200).json(res.locals.volumes)
 })
 
+volumeRouter.delete('/all-volumes', volumeController.deleteAllVolumes, (_req: Request, res: Response) => {
+    res.status(200).json('res.locals.deleted')
+})
 export default volumeRouter
