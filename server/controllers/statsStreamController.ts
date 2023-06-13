57fdce5e36c79c315521b3501b7cff71e45fc51d
Steve Lemlek <stevelemlek@gmail.com>
2023-06-13 11:36:27 -0500

    streamcontroller promisfied and tested

--- server/controllers/statsStreamController.ts
+++ server/controllers/statsStreamController.ts
@@ -1,30 +1,35 @@
-import { Request, Response, NextFunction } from "express";
-import { exec } from "node:child_process";
-import { StatsStreamController } from "../../types";
+import { Request, Response, NextFunction } from 'express';
+import { exec } from 'node:child_process';
+import { StatsStreamController, ErrorDetails } from '../../types';
+import { promisify } from 'node:util';
+const promisifyExec = promisify(exec);
 
 const statsStreamController: StatsStreamController = {
-  getAllContainerStats: async (
-    _req: Request,
-    res: Response,
-    next: NextFunction
-  ) => {
-    try {
-      await exec(
-        "docker stats --no-stream --format json",
-        (error, stdout, stderr) => {
-          if (error) {
-            return res.status(401);
-          }
-          return res.status(200).json(stdout || stderr);
-        }
-      );
-    } catch (error) {
-      next({
-        log: "error in the generalDockerMiddleware",
-        err: error,
-      });
-    }
-  },
+	getAllContainerStats: async (
+		_req: Request,
+		res: Response,
+		next: NextFunction
+	) => {
+		try {
+			const { stdout, stderr } = await promisifyExec(
+				'docker stats --no-stream --format json'
+			);
+			if (stderr) {
+				const errorDetails: ErrorDetails = {
+					log: 'error in the generalDockerMiddleware exec call',
+					err: stderr,
+					message: 'error in the generalDockerMiddleware exec call',
+				};
+				next(errorDetails);
+			}
+			return res.status(200).json(stdout);
+		} catch (error) {
+			next({
+				log: 'error in the generalDockerMiddleware',
+				err: error,
+			});
+		}
+	},
 };
 
 export default statsStreamController;
