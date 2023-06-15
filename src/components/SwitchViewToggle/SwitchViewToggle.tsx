672b3456f7704c37d4b67d4bff0096a8f9a2d795
user.email <williaminterlock@gmail.com>
2023-06-15 10:10:56 -0500

    unused imports

--- src/components/SwitchViewToggle/SwitchViewToggle.tsx
+++ src/components/SwitchViewToggle/SwitchViewToggle.tsx
@@ -1,5 +1,5 @@
-import React,{useContext} from 'react';
-import * as ToggleGroup from '@radix-ui/react-toggle-group';
-import { UserContext } from '../../UserContext';
+// import React,{useContext} from 'react';
+// import * as ToggleGroup from '@radix-ui/react-toggle-group';
+// import { UserContext } from '../../UserContext';
 
 //this is a component that will switch the views that are in main nav by manipulating the state of setShowImages and setShowContainers
