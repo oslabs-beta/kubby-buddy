7885238bf19450984c6daa08955d6fbb5bd16786
YeChanGoo <126903033+YeChanGoo@users.noreply.github.com>
2023-06-15 19:08:30 -0400

    fix: add port route and combine all POST into one

--- server/controllers/imageController.ts
+++ server/controllers/imageController.ts
@@ -43,10 +43,65 @@ const imageController: ImageController = {
     res: Response,
     next: NextFunction
   ): Promise<void> => {
-    const { name, image } = req.body;
+    const { name, image, remove, volumeName, fileDirectory, port } = req.body;
+    let rm;
+    let vol;
+    let portNum;
+
+    // check if name and image is provided
+    if (!name || !image || name.trim() === '' || image.trim() === '') {
+      const errorDetails: ErrorDetails = {
+        log: 'error in containerController.runContainerFromImage',
+        err: null,
+        message: 'Missing name or image fields',
+      };
+      return next(errorDetails);
+    }
+
+    // check if remove is provided
+    if (remove === 'yes') {
+      rm = '--rm';
+    } else {
+      rm = '';
+    }
+
+    // check if volume is provided
+    if (volumeName && volumeName.trim() !== '') {
+      // check if fileDirectory is provided
+      if (fileDirectory && fileDirectory.trim() !== '') {
+        vol = `-v ${volumeName}:${fileDirectory}`;
+      } else {
+        vol = `-v ${volumeName}:/App`;
+      }
+    } else {
+      vol = '';
+    }
+
+    // check if port is provided
+    if (port && port.trim() !== '') {
+      try {
+        const { stdout } = await promisifyExec(
+          `docker inspect --format='{{.Config.ExposedPorts}}' ${image}`
+        );
+        const regex = /\[(\d+)\//;
+        const match = stdout.match(regex);
+        const result = match ? match[1] : null;
+        portNum = `-p ${port}:${result}`;
+      } catch (error) {
+        const errorDetails: ErrorDetails = {
+          log: 'error in imageController.runContainerFromImage PORT catch',
+          err: error,
+          message: `error in exec of imageController.runContainerFromImage PORT catch`,
+        };
+        next(errorDetails);
+      }
+    } else {
+      portNum = '';
+    }
+
     try {
       const { stdout, stderr } = await promisifyExec(
-        `docker run -d --name ${name} ${image}`
+        `docker run -d ${rm} ${vol} ${portNum} --name ${name} ${image}`
       );
       if (stderr) {
         const errorDetails: ErrorDetails = {
@@ -69,104 +124,135 @@ const imageController: ImageController = {
     }
   },
 
-  // run container with remove when it stops
-  runContainerFromImageWithRemove: async (
-    req: Request,
-    res: Response,
-    next: NextFunction
-  ): Promise<void> => {
-    const { name, image } = req.body;
-    try {
-      const { stdout, stderr } = await promisifyExec(
-        `docker run -d --rm --name ${name} ${image}`
-      );
-      if (stderr) {
-        const errorDetails: ErrorDetails = {
-          log: 'error in the imageController.runContainerFromImage exec',
-          err: stderr,
-          message: 'error in the imageController.runContainerFromImage exec',
-        };
-        next(errorDetails);
-      }
-      res.locals.ranContainerWithRemove = [{ message: stdout.trim() }];
-      next();
-    } catch (error) {
-      const errorDetails: ErrorDetails = {
-        log: 'error in imageController.runContainerFromImageWithRemove catch',
-        err: error,
-        message: `error in exec of imageController.runContainerFromImageWithRemove catch`,
-      };
-      next(errorDetails);
-    }
-  },
+  // runContainerFromImage: async (
+  //   req: Request,
+  //   res: Response,
+  //   next: NextFunction
+  // ): Promise<void> => {
+  //   const { name, image } = req.body;
+  //   try {
+  //     const { stdout, stderr } = await promisifyExec(
+  //       `docker run -d --name ${name} ${image}`
+  //     );
+  //     if (stderr) {
+  //       const errorDetails: ErrorDetails = {
+  //         log: 'error in the imageController.runContainerFromImage exec',
+  //         err: stderr,
+  //         message: 'error in the imageController.runContainerFromImage exec',
+  //       };
+  //       next(errorDetails);
+  //     }
+  //     const output = stdout.trim();
+  //     res.locals.ranContainer = [{ message: output }];
+  //     next();
+  //   } catch (error) {
+  //     const errorDetails: ErrorDetails = {
+  //       log: 'error in imageController.runContainerFromImage catch',
+  //       err: error,
+  //       message: `error in exec of imageController.runContainerFromImage catch`,
+  //     };
+  //     next(errorDetails);
+  //   }
+  // },
 
-  // run container with named volume
-  runContainerFromImageWithNamedVolume: async (
-    req: Request,
-    res: Response,
-    next: NextFunction
-  ): Promise<void> => {
-    const { name, image, volumeName, fileDirectory } = req.body;
-    try {
-      const { stdout, stderr } = await promisifyExec(
-        `docker run -d --name ${name} -v ${volumeName}:${fileDirectory} ${image}`
-      );
-      if (stderr) {
-        const errorDetails: ErrorDetails = {
-          log: 'error in the imageController.runContainerFromImageWithNamedVolume exec',
-          err: stderr,
-          message:
-            'error in the imageController.runContainerFromImageWithNamedVolume exec',
-        };
-        next(errorDetails);
-      }
-      res.locals.ranContainerFromImageWithNamedVolume = [
-        { message: stdout.trim() },
-      ];
-      next();
-    } catch (error) {
-      const errorDetails: ErrorDetails = {
-        log: 'error in imageController.unContainerFromImageWithNamedVolume catch',
-        err: error,
-        message: `error in exec of imageController.unContainerFromImageWithNamedVolume catch`,
-      };
-      next(errorDetails);
-    }
-  },
+  // // run container with remove when it stops
+  // runContainerFromImageWithRemove: async (
+  //   req: Request,
+  //   res: Response,
+  //   next: NextFunction
+  // ): Promise<void> => {
+  //   const { name, image } = req.body;
+  //   try {
+  //     const { stdout, stderr } = await promisifyExec(
+  //       `docker run -d --rm --name ${name} ${image}`
+  //     );
+  //     if (stderr) {
+  //       const errorDetails: ErrorDetails = {
+  //         log: 'error in the imageController.runContainerFromImage exec',
+  //         err: stderr,
+  //         message: 'error in the imageController.runContainerFromImage exec',
+  //       };
+  //       next(errorDetails);
+  //     }
+  //     res.locals.ranContainerWithRemove = [{ message: stdout.trim() }];
+  //     next();
+  //   } catch (error) {
+  //     const errorDetails: ErrorDetails = {
+  //       log: 'error in imageController.runContainerFromImageWithRemove catch',
+  //       err: error,
+  //       message: `error in exec of imageController.runContainerFromImageWithRemove catch`,
+  //     };
+  //     next(errorDetails);
+  //   }
+  // },
 
-  // run container with named volume and remove
-  runContainerFromImageWithNamedVolumeAndRemove: async (
-    req: Request,
-    res: Response,
-    next: NextFunction
-  ): Promise<void> => {
-    const { name, image, volumeName, fileDirectory } = req.body;
-    try {
-      const { stdout, stderr } = await promisifyExec(
-        `docker run -d --rm --name ${name} -v ${volumeName}:${fileDirectory} ${image}`
-      );
-      if (stderr) {
-        const errorDetails: ErrorDetails = {
-          log: 'error in the imageController.runContainerFromImageWithNamedVolumeAndRemove exec',
-          err: stderr,
-          message:
-            'error in the imageController.runContainerFromImageWithNamedVolumeAndRemove exec',
-        };
-        next(errorDetails);
-      }
-      res.locals.ranContainerFromImageWithNamedVolumeAndRemove = [
-        { message: stdout.trim() },
-      ];
-      next();
-    } catch (error) {
-      const errorDetails: ErrorDetails = {
-        log: 'error in imageController.runContainerFromImageWithNamedVolumeAndRemove catch',
-        err: error,
-        message: `error in exec of imageController.runContainerFromImageWithNamedVolumeAndRemove catch`,
-      };
-      next(errorDetails);
-    }
-  },
+  // // run container with named volume
+  // runContainerFromImageWithNamedVolume: async (
+  //   req: Request,
+  //   res: Response,
+  //   next: NextFunction
+  // ): Promise<void> => {
+  //   const { name, image, volumeName, fileDirectory } = req.body;
+  //   try {
+  //     const { stdout, stderr } = await promisifyExec(
+  //       `docker run -d --name ${name} -v ${volumeName}:${fileDirectory} ${image}`
+  //     );
+  //     if (stderr) {
+  //       const errorDetails: ErrorDetails = {
+  //         log: 'error in the imageController.runContainerFromImageWithNamedVolume exec',
+  //         err: stderr,
+  //         message:
+  //           'error in the imageController.runContainerFromImageWithNamedVolume exec',
+  //       };
+  //       next(errorDetails);
+  //     }
+  //     res.locals.ranContainerFromImageWithNamedVolume = [
+  //       { message: stdout.trim() },
+  //     ];
+  //     next();
+  //   } catch (error) {
+  //     const errorDetails: ErrorDetails = {
+  //       log: 'error in imageController.unContainerFromImageWithNamedVolume catch',
+  //       err: error,
+  //       message: `error in exec of imageController.unContainerFromImageWithNamedVolume catch`,
+  //     };
+  //     next(errorDetails);
+  //   }
+  // },
+
+  // // run container with named volume and remove
+  // runContainerFromImageWithNamedVolumeAndRemove: async (
+  //   req: Request,
+  //   res: Response,
+  //   next: NextFunction
+  // ): Promise<void> => {
+  //   const { name, image, volumeName, fileDirectory } = req.body;
+  //   try {
+  //     const { stdout, stderr } = await promisifyExec(
+  //       `docker run -d --rm --name ${name} -v ${volumeName}:${fileDirectory} ${image}`
+  //     );
+  //     if (stderr) {
+  //       const errorDetails: ErrorDetails = {
+  //         log: 'error in the imageController.runContainerFromImageWithNamedVolumeAndRemove exec',
+  //         err: stderr,
+  //         message:
+  //           'error in the imageController.runContainerFromImageWithNamedVolumeAndRemove exec',
+  //       };
+  //       next(errorDetails);
+  //     }
+  //     res.locals.ranContainerFromImageWithNamedVolumeAndRemove = [
+  //       { message: stdout.trim() },
+  //     ];
+  //     next();
+  //   } catch (error) {
+  //     const errorDetails: ErrorDetails = {
+  //       log: 'error in imageController.runContainerFromImageWithNamedVolumeAndRemove catch',
+  //       err: error,
+  //       message: `error in exec of imageController.runContainerFromImageWithNamedVolumeAndRemove catch`,
+  //     };
+  //     next(errorDetails);
+  //   }
+  // },
 
   // prune unused images (ones not actively connected with a container)
   pruneUnusedImages: async (
--- server/routes/imageRoutes.ts
+++ server/routes/imageRoutes.ts
@@ -15,10 +15,14 @@ imageRouter.get(
   }
 );
 
+/**
 // post route to run a single container from an image
-// INPUT: object with name and image value, {"name": "mongodb", "image": "mongo"}
+// INPUT: object with name, image, value, remove, volumeName, fileDirectory, port values 
+  {"name": "container1", "image": "mongo", "remove": "yes", "volumeName": "vol1", "fileDirectory": "/App", "port": "3000"}
 // OUTPUT: array of object containing ID of container [{"message": "cf29432706cdabfd550b37561311d8f56fe18c8de25ae0f1069d501900ecc49a"}]
 // other tests: container actually runs... maybe you can use another route to test?
+*/
+
 imageRouter.post(
   '/run-images',
   imageController.runContainerFromImage,
@@ -31,37 +35,40 @@ imageRouter.post(
 // INPUT: object with name and image value, {"name": "mongodb", "image": "mongo"}
 // OUTPUT: array of object containing ID of container [{"message": "cf29432706cdabfd550b37561311d8f56fe18c8de25ae0f1069d501900ecc49a"}]
 // other tests: container actually runs... maybe you can use another route to test?
-imageRouter.post(
-  '/run-images-with-remove',
-  imageController.runContainerFromImageWithRemove,
-  (_req: Request, res: Response) => {
-    res.status(200).json(res.locals.ranContainerWithRemove);
-  }
-);
+
+// imageRouter.post(
+//   '/run-images-with-remove',
+//   imageController.runContainerFromImageWithRemove,
+//   (_req: Request, res: Response) => {
+//     res.status(200).json(res.locals.ranContainerWithRemove);
+//   }
+// );
 
 // post route to run a container from an image that creates named volume
 // INPUT: object with name, image, volumeName, fileDirectory, {"name": "mongodb", "image": "mongo", "volumeName": "vol1", "fileDirectory": "/App"}
 // OUTPUT: array of object that has message with value of running container, [{"message": "fbc113b8031af3560c77f023ea7fce22a8f7ff3c5c958b3b9c158956fb8e85ed"}]
-imageRouter.post(
-  '/run-images-with-named-volume',
-  imageController.runContainerFromImageWithNamedVolume,
-  (_req: Request, res: Response) => {
-    res.status(200).json(res.locals.ranContainerFromImageWithNamedVolume);
-  }
-);
+
+// imageRouter.post(
+//   '/run-images-with-named-volume',
+//   imageController.runContainerFromImageWithNamedVolume,
+//   (_req: Request, res: Response) => {
+//     res.status(200).json(res.locals.ranContainerFromImageWithNamedVolume);
+//   }
+// );
 
 // post route to run a container from an image that creates named volume with remove
 // INPUT: object with name, image, volumeName, fileDirectory, {"name": "mongodb", "image": "mongo", "volumeName": "vol1", "fileDirectory": "/App"}
 // OUTPUT: array of object that has message with value of running container, [{"message": "fbc113b8031af3560c77f023ea7fce22a8f7ff3c5c958b3b9c158956fb8e85ed"}]
-imageRouter.post(
-  '/run-images-with-named-volume-and-remove',
-  imageController.runContainerFromImageWithNamedVolumeAndRemove,
-  (_req: Request, res: Response) => {
-    res
-      .status(200)
-      .json(res.locals.ranContainerFromImageWithNamedVolumeAndRemove);
-  }
-);
+
+// imageRouter.post(
+//   '/run-images-with-named-volume-and-remove',
+//   imageController.runContainerFromImageWithNamedVolumeAndRemove,
+//   (_req: Request, res: Response) => {
+//     res
+//       .status(200)
+//       .json(res.locals.ranContainerFromImageWithNamedVolumeAndRemove);
+//   }
+// );
 
 // delete all unused images (ones not acively connected with a container)
 // INPUT: nothing
