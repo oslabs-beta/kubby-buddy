ef87d78f7820abd1beaae78d38f0ee4f3db55540
Steve Lemlek <stevelemlek@gmail.com>
2023-06-19 11:04:04 -0500

    fix: updated graffana embed url

--- src/components/Mainnav/MainNav.tsx
+++ src/components/Mainnav/MainNav.tsx
@@ -16,7 +16,7 @@ export const MainNav: FC = () => {
       <ul>
         <iframe
           className="grafana"
-          src="http://localhost:3001/d-solo/c6d98103-49a2-4a71-a199-144325115d9f/new-dashboard?orgId=1&from=1687134778649&to=1687135678649&panelId=1"
+          src="http://localhost:3001/d-solo/c6d98103-49a2-4a71-a199-144325115d9f/new-dashboard?orgId=1&from=1687188879695&to=1687189779695&panelId=1"
         ></iframe>
         {showDockerContainers && !showImages ? (
           <DockerContainers />
