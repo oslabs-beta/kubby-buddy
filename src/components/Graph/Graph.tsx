fb066a9ced597d6c42ee05a52ed79dc58f0782e7
Steve Lemlek <stevelemlek@gmail.com>
2023-06-18 21:08:18 -0500

    feat: bar chart working, chart import figured out

--- src/components/Graph/Graph.tsx
+++ src/components/Graph/Graph.tsx
@@ -1,6 +1,44 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import './Graph.scss';
+import { Bar } from 'react-chartjs-2';
+import 'chart.js/auto';
 
 export const Graph: FC = () => {
-  return <div className="graph"></div>;
+  const [userData, setUserData] = useState({
+    labels: ['CPU'],
+    datasets: [
+      {
+        label: '%',
+        data: [80],
+      },
+    ],
+  });
+
+  if (10 > 11) {
+    setUserData({
+      labels: ['CPU'],
+      datasets: [
+        {
+          label: '%',
+          data: [80],
+        },
+      ],
+    });
+  }
+  return (
+    <div className="graph">
+      <Bar
+        data={userData}
+        options={{
+          scales: {
+            y: {
+              stacked: true,
+              min: 0,
+              max: 100,
+            },
+          },
+        }}
+      />
+    </div>
+  );
 };
