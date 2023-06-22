450f1f153b13676cfb95be07c3174839b02be980
Kasu <kasu@kasu-it.dev>
2023-06-21 19:51:12 -0500

    fix: map errors with fetching images and containers when none are ruinning

--- src/components/Container/ContainerDisplay.tsx
+++ src/components/Container/ContainerDisplay.tsx
@@ -9,91 +9,97 @@ import { Graph } from '../Graph/Graph';
 export const DisplayRunning: FC = () => {
   const { runningContainers, statStream } = useContext(UserContext);
   const [stopInvoked, setStop] = useState(false);
-  console.log('testtest====', runningContainers);
 
   const handleStopInvoke = () => {
     if (!stopInvoked) setStop(true);
     else setStop(false);
   };
 
-  console.log('hi', statStream[0]);
-  return (
-    <div className="dockercontainer">
-      {runningContainers.map((el, index) => (
-        <div className="container" key={index}>
-          <div className="container-info">
-            <div className="container-name">{el.Names}</div>
-            <div className="subinfo">
-              <p className="Imagename">Image: {el.Image}</p>
-              <p className="Port">Port: {el.Ports}</p>
-              {/* <p className="State">State: {el.State}</p>
-              <p className="Networks">Networks: {el.Networks}</p>
-              <p className="CreatedAt">CreatedAt: {el.CreatedAt}</p>
-              <p className="Status">Status: {el.Status}</p>
-              <p className="Size">Size: {el.Size}</p>
-              <p className="Command">Command: {el.Command}</p>
-              <p className="ID">ID: {el.ID}</p>
-              <p className="LocalVolumes">LocalVolumes: {el.LocalVolumes}</p>
-              <p className="Mounts">Mounts: {el.Mounts}</p>
-              <p className="Labels">Labels: {el.Labels}</p>
-              <p className="volume">volume: {(el.volume as string) || "N/A"}</p> */}
-            </div>
-          </div>
+  let containers;
 
-          <div className="cmdbutton">
-            <StartCommands
-              name={el.Names}
-              cmdRoute={new URL('/container/start', window.location.href)}
-              fetchMethod="post"
-              onClick={handleStopInvoke}
-            />
-            {/* <StopCommands name={el.Name} /> */}
-            <StopCommands
-              name={el.Names}
-              cmdRoute={new URL('/container/stop', window.location.href)}
-              fetchMethod="post"
-              // onClick={handleStopInvoke}
-            />
-            {/* {stopInvoked && (
-                <DeleteCommands
-                 name={el.Names}
-                 cmdRoute={new URL("/container/remove-specific-container", window.location.href)}
-                 fetchMethod='delete'
-                />
-
-            )} */}
-            <DeleteCommands
-              name={el.Names}
-              cmdRoute={
-                new URL(
-                  '/container/remove-specific-container',
-                  window.location.href
-                )
-              }
-              fetchMethod="delete"
-            />
-            <LogCommands
-              name={el.Names}
-              cmdRoute={new URL('/container/log', window.location.href)}
-              fetchMethod="get"
-            />
-          </div>
-
-          {/* <div className="dropdown">
-            <button>container</button>
-            <button>volumes</button>
-            <button>image</button>
-            <button>stats</button>
-          </div> */}
-          <div className="chartContainer">
-            {statStream.length > 0 ? (
-              <Graph className="bargraph" data={statStream[index]} />
-            ) : (
-              ''
-            )}
+  if (typeof runningContainers === 'string') {
+    containers = (
+      <div>
+        <p>You have no running containers</p>
+      </div>
+    );
+  } else {
+    containers = runningContainers.map((el, index) => (
+      <div className="container" key={index}>
+        <div className="container-info">
+          <div className="container-name">{el.Names}</div>
+          <div className="subinfo">
+            <p className="Imagename">Image: {el.Image}</p>
+            <p className="Port">Port: {el.Ports}</p>
+            {/* <p className="State">State: {el.State}</p>
+          <p className="Networks">Networks: {el.Networks}</p>
+          <p className="CreatedAt">CreatedAt: {el.CreatedAt}</p>
+          <p className="Status">Status: {el.Status}</p>
+          <p className="Size">Size: {el.Size}</p>
+          <p className="Command">Command: {el.Command}</p>
+          <p className="ID">ID: {el.ID}</p>
+          <p className="LocalVolumes">LocalVolumes: {el.LocalVolumes}</p>
+          <p className="Mounts">Mounts: {el.Mounts}</p>
+          <p className="Labels">Labels: {el.Labels}</p>
+          <p className="volume">volume: {(el.volume as string) || "N/A"}</p> */}
           </div>
         </div>
-      ))}
-    </div>
-  );
+
+        <div className="cmdbutton">
+          <StartCommands
+            name={el.Names}
+            cmdRoute={new URL('/container/start', window.location.href)}
+            fetchMethod="post"
+            onClick={handleStopInvoke}
+          />
+          {/* <StopCommands name={el.Name} /> */}
+          <StopCommands
+            name={el.Names}
+            cmdRoute={new URL('/container/stop', window.location.href)}
+            fetchMethod="post"
+            // onClick={handleStopInvoke}
+          />
+          {/* {stopInvoked && (
+            <DeleteCommands
+             name={el.Names}
+             cmdRoute={new URL("/container/remove-specific-container", window.location.href)}
+             fetchMethod='delete'
+            />
+
+        )} */}
+          <DeleteCommands
+            name={el.Names}
+            cmdRoute={
+              new URL(
+                '/container/remove-specific-container',
+                window.location.href
+              )
+            }
+            fetchMethod="delete"
+          />
+          <LogCommands
+            name={el.Names}
+            cmdRoute={new URL('/container/log', window.location.href)}
+            fetchMethod="get"
+          />
+        </div>
+
+        {/* <div className="dropdown">
+        <button>container</button>
+        <button>volumes</button>
+        <button>image</button>
+        <button>stats</button>
+      </div> */}
+        <div className="chartContainer">
+          {statStream.length > 0 ? (
+            <Graph className="bargraph" data={statStream[index]} />
+          ) : (
+            ''
+          )}
+        </div>
+      </div>
+    ));
+  }
+
+  return <div className="dockercontainer">{containers}</div>;
 };
--- src/components/Images/Images.tsx
+++ src/components/Images/Images.tsx
@@ -6,30 +6,38 @@ import { CreateCommands } from '../../components/Button/Create';
 export const Images: FC = () => {
   const { availableImages } = useContext(UserContext);
 
-  return (
-    <div className="imagescontainer">
-      {availableImages.map((el, index) => (
-        <li className="listImage" key={index}>
-          <div className="image-info">
-            <p>{el.Repository}</p>
-            <div className="image-subinfo">
-              <p>Containers: {el.Containers}</p>
-              <p>Time since created: {el.CreatedSince}</p>
-              <p>Created At: {el.CreatedAt}</p>
-              <p>Size: {el.Size}</p>
-              <p>Tag: {el.Tag}</p>
-              <p>ID: {el.ID}</p>
-            </div>
+  let images;
+
+  if (typeof availableImages === 'string') {
+    images = (
+      <div>
+        <p>You have no running images</p>
+      </div>
+    );
+  } else {
+    images = availableImages.map((el, index) => (
+      <li className="listImage" key={index}>
+        <div className="image-info">
+          <p>{el.Repository}</p>
+          <div className="image-subinfo">
+            <p>Containers: {el.Containers}</p>
+            <p>Time since created: {el.CreatedSince}</p>
+            <p>Created At: {el.CreatedAt}</p>
+            <p>Size: {el.Size}</p>
+            <p>Tag: {el.Tag}</p>
+            <p>ID: {el.ID}</p>
           </div>
-          <div className="cmdbutton">
-            <CreateCommands
-              name={el.Repository}
-              cmdRoute={new URL('/image/run-images', window.location.href)}
-              fetchMethod="post"
-            />
-          </div>
-        </li>
-      ))}
-    </div>
-  );
+        </div>
+        <div className="cmdbutton">
+          <CreateCommands
+            name={el.Repository}
+            cmdRoute={new URL('/image/run-images', window.location.href)}
+            fetchMethod="post"
+          />
+        </div>
+      </li>
+    ));
+  }
+
+  return <div className="imagescontainer">{images}</div>;
 };
