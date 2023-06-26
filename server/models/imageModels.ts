1d5832336553a4247567537d975a4db9fb112620
Steve Lemlek <stevelemlek@gmail.com>
2023-06-26 11:03:21 -0500

    feat: models folder and pool created

---
+++ server/models/imageModels.ts
@@ -0,0 +1,15 @@
+import { Pool } from 'pg';
+
+const PG_URI =
+  'postgres://miprnoed:LAZstS_z_xZhHFeZsDib4Kv5FgdEiUJ9@rajje.db.elephantsql.com/miprnoed';
+
+const pool = new Pool({
+  connectionString: PG_URI,
+});
+
+export default {
+  query: (text: string, params: any, callback: any) => {
+    console.log('executed query', text);
+    return pool.query(text, params, callback);
+  },
+};
