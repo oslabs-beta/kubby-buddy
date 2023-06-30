7b010c9a88e5aecde8663b91c669bb57175a764c
YeChanGoo <126903033+YeChanGoo@users.noreply.github.com>
2023-06-30 13:21:21 -0400

    feat: added testing for /all-active-containers

---
+++ __tests__/routes/containerController.test.ts
@@ -0,0 +1,91 @@
+// import { Docker } from 'node-docker-api';
+import express from 'express';
+import containerRouter from '../../server/routes/containerRoutes';
+// import request from 'supertest';
+import { Server } from 'http';
+
+import { execSync } from 'child_process';
+import {
+  cmdGetAllRunningContainers,
+  // cmdGetAllRunningContainersNames,
+  // cmdStopASpecificContainer,
+  // cmdStartASpecificContainer,
+  // cmdPruneStoppedContainers,
+  // cmdGetSpecificLog,
+  // cmdRemoveSpecificContainer,
+  parseOutputContainers,
+  // parseOutputContainersNames,
+  // parseOutputStartStop,
+  // parseOutputPruneStoppedContainers,
+  // transformLogs,
+  // parseOutputRemoveSpecificContainer,
+} from '../../server/controllers/containerController';
+
+const app = express();
+app.use('/', containerRouter);
+let server: Server;
+
+beforeEach(async () => {
+  server = app.listen(3200); // Start the server
+
+  // Start the containers
+  execSync(
+    'docker exec -i DIND sh -c "docker run -d --name=my-container alpine sleep 3600 && docker run -d --name=my-container2 alpine sleep 3600"'
+  );
+
+  // Delay for a certain amount of time to allow the containers to start
+  // await new Promise((resolve) => setTimeout(resolve, 3000));
+});
+
+afterEach(async () => {
+  server.close();
+
+  execSync(
+    `docker exec -i DIND sh -c 'docker rm -f my-container; docker rm -f my-container2'`
+  );
+});
+
+describe('Docker Tests', () => {
+  test('GET /all-active-containers should return an array of containers', async () => {
+    // Get the list of active containers within the DIND container
+    execSync(`docker exec -i DIND sh -c "docker stop my-container"`, {
+      stdio: 'pipe',
+    });
+    // console.log('TEST1', test1.toString());
+    // Get the list of running containers
+    const dindContainersResponse = execSync(
+      `docker exec -i DIND sh -c "${cmdGetAllRunningContainers}"`,
+      {
+        stdio: 'pipe',
+      }
+    );
+
+    const dindContainers = parseOutputContainers(dindContainersResponse);
+    // console.log(
+    //   'dindContainers:',
+    //   parseOutputContainers(dindContainersResponse)
+    // );
+
+    // Make sure the my-container is running within the DIND container
+    expect(
+      dindContainers.some(
+        (container) =>
+          container.Names === 'my-container' &&
+          container.Status.includes('Exited')
+      )
+    ).toBeTruthy();
+    expect(
+      dindContainers.some(
+        (container) =>
+          container.Names === 'my-container2' && container.Status.includes('Up')
+      )
+    ).toBeTruthy();
+    expect(dindContainers).toHaveLength(2);
+    expect(
+      dindContainers.some((container) => container.name === 'hungry_khorana')
+    ).toBeFalsy();
+    expect(Array.isArray(dindContainers)).toBeTruthy();
+    expect(dindContainers.length).toBeGreaterThan(0);
+    expect(typeof dindContainers[0]).toBe('object');
+  });
+});
--- jest.config.ts
+++ jest.config.ts
@@ -1,7 +1,12 @@
-import type { Config } from 'jest';
+import { Config } from '@jest/types';
 
-export default async (): Promise<Config> => {
+export default async (): Promise<Config.InitialOptions> => {
   return {
     verbose: true,
+    transform: {
+      '^.+\\.tsx?$': 'ts-jest',
+    },
+    moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
+    testMatch: ['**/__tests__/**/*.test.(js|jsx|ts|tsx)'],
   };
 };
