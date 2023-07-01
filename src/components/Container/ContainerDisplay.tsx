34f5ee9a338e446325c80d42fbc2bdd3d988bf03
user.email <williaminterlock@gmail.com>
2023-07-01 14:26:13 -0500

    fix: chart properly updating

--- src/components/Container/ContainerDisplay.tsx
+++ src/components/Container/ContainerDisplay.tsx
@@ -1,25 +1,20 @@
 import React, { FC, useContext, useEffect, useState } from 'react';
 import { UserContext } from '../../UserContext';
-import { StartCommands } from '../Button/Start';
+
 import { StopCommands } from '../Button/Stop';
-import { DeleteCommands } from '../Button/Delete';
+
 import { LogCommands } from '../Button/Logs';
+import Loader from '../Loader/Loader';
 // import { Graph } from '../Graph/Graph';
 import LineGraph from '../LineGraph/Line';
 import Donut1 from '../Donut/DonutCPU';
-import Loader from '../Loader/Loader';
+
 import Donut2 from '../Donut/DonutMemory';
 
 export const DisplayRunning: FC = () => {
   const { runningContainers, statStream } = useContext(UserContext);
-  const [stopInvoked, setStop] = useState(false);
-  const [change, setChange] = useState(false);
-  // console.log('testtest====', runningContainers);
 
-  const handleStopInvoke = () => {
-    if (!stopInvoked) setStop(true);
-    else setStop(false);
-  };
+  const [change, setChange] = useState(false);
 
   const updateChange = () => {
     setChange((prevChange) => !prevChange);
@@ -31,114 +26,57 @@ export const DisplayRunning: FC = () => {
     console.log(runningContainers);
   }, [runningContainers, statStream]);
 
-  let running;
-
-  if (typeof runningContainers === 'string') {
-    running = <Loader />;
-  } else {
-    running = runningContainers.map((el, index) => (
-      <div className="container" key={index}>
-        <div className="container-info">
-          <div className="container-name">{el.Names}</div>
-          <div className="subinfo">
-            <p className="Imagename">Image: {el.Image}</p>
-            <p className="Port">Port: {el.Ports}</p>
-            {/* <p className="State">State: {el.State}</p>
-          <p className="Networks">Networks: {el.Networks}</p>
-          <p className="CreatedAt">CreatedAt: {el.CreatedAt}</p>
-          <p className="Status">Status: {el.Status}</p>
-          <p className="Size">Size: {el.Size}</p>
-          <p className="Command">Command: {el.Command}</p>
-          <p className="ID">ID: {el.ID}</p>
-          <p className="LocalVolumes">LocalVolumes: {el.LocalVolumes}</p>
-          <p className="Mounts">Mounts: {el.Mounts}</p>
-          <p className="Labels">Labels: {el.Labels}</p>
-          <p className="volume">volume: {(el.volume as string) || "N/A"}</p> */}
-          </div>
+  let running = runningContainers.map((el, index) => (
+    <div className="container" key={index}>
+      <div className="container-info">
+        <div className="container-name">{el.Names}</div>
+        <div className="subinfo">
+          <p className="Imagename">Image: {el.Image}</p>
+          <p className="Port">Port: {el.Ports}</p>
         </div>
-
-        <div className="cmdbutton">
-          <StartCommands
-            name={el.Names}
-            cmdRoute={new URL('/container/start', window.location.href)}
-            fetchMethod="post"
-            onClick={handleStopInvoke}
-          />
-          {/* <StopCommands name={el.Name} /> */}
-          <StopCommands
-            name={el.Names}
-            cmdRoute={new URL('/container/stop', window.location.href)}
-            fetchMethod="post"
-            // onClick={handleStopInvoke}
-          />
-          {/* {stopInvoked && (
-            <DeleteCommands
-             name={el.Names}
-             cmdRoute={new URL("/container/remove-specific-container", window.location.href)}
-             fetchMethod='delete'
-            />
-
-        )} */}
-          <DeleteCommands
-            name={el.Names}
-            cmdRoute={
-              new URL(
-                '/container/remove-specific-container',
-                window.location.href
-              )
-            }
-            fetchMethod="delete"
-          />
-          <LogCommands
-            name={el.Names}
-            cmdRoute={
-              new URL(`/container/log?name=${el.Names}`, window.location.href)
-            }
-            fetchMethod="get"
-          />
-        </div>
-
-        {/* <div className="dropdown">
-        <button>container</button>
-        <button>volumes</button>
-        <button>image</button>
-        <button>stats</button>
-      </div> */}
-        <div className="chartContainer">
-          {statStream.length > 0 ? (
-            <Donut1 className="bargraph" data={statStream[index]} />
-          ) : (
-            ''
-          )}
-        </div>
-        <div className="chartContainer">
-          {statStream.length > 0 ? (
-            <Donut2 className="bargraph" data={statStream[index]} />
-          ) : (
-            ''
-          )}
-        </div>
-        <div className="chartContainer">
-          {statStream.length > 0 ? (
-            <LineGraph
-              className="bargraph"
-              data={statStream[index]}
-              change={change}
-            />
-          ) : (
-            ''
-          )}
-        </div>
-        {/* <div className="chartContainer">
-            {statStream.length > 0 ? (
-              <Graph className="bargraph" data={statStream[index]} />
-            ) : (
-              ''
-            )}
-          </div> */}
       </div>
-    ));
-  }
+
+      <div className="cmdbutton">
+        <StopCommands
+          name={el.Names}
+          cmdRoute={new URL('/container/stop', window.location.href)}
+          fetchMethod="post"
+        />
+        <LogCommands
+          name={el.Names}
+          cmdRoute={
+            new URL(`/container/log?name=${el.Names}`, window.location.href)
+          }
+          fetchMethod="get"
+        />
+      </div>
+      <div className="chartContainer">
+        {statStream.length > 0 ? (
+          <Donut1 className="bargraph" data={statStream[index]} />
+        ) : (
+          ''
+        )}
+      </div>
+      <div className="chartContainer">
+        {statStream.length > 0 ? (
+          <Donut2 className="bargraph" data={statStream[index]} />
+        ) : (
+          <Loader />
+        )}
+      </div>
+      <div className="chartContainer">
+        {statStream.length > 0 ? (
+          <LineGraph
+            className="bargraph"
+            data={statStream[index]}
+            change={change}
+          />
+        ) : (
+          ''
+        )}
+      </div>
+    </div>
+  ));
 
   return <div className="dockercontainer">{running}</div>;
 };
