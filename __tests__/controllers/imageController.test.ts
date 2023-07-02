ff13a0b3400cb64cdde577f2e1466f544ede03a9
YeChanGoo <126903033+YeChanGoo@users.noreply.github.com>
2023-07-02 17:41:22 -0400

    feat: added tests for imageController

--- __tests__/controllers/imageController.test.ts
+++ __tests__/controllers/imageController.test.ts
@@ -8,11 +8,22 @@ app.use('/', containerRouter);
 let server: Server;
 import {
   cmdGetAllImages,
+  cmdRunContainerFromImage,
+  cmdRunContainerFromImageName,
+  cmdPruneUnusedImages,
+  cmdRemoveSingleImage,
+  parseOutputContainers,
   parseOutputGetAllImages,
+  setRmOption,
+  setVolOption,
+  // getPortMapping,
+  parseOutputrunContainerFromImage,
+  parseOutputPruneUnusedImages,
+  parseOutputRemoveSingleImage,
 } from '../../server/controllers/imageController';
 
 beforeEach(async () => {
-  server = app.listen(3300); // Start the server
+  server = app.listen(9001); // Start the server
 
   // Start the containers
   execSync(
@@ -34,7 +45,7 @@ afterEach(async () => {
 describe('imageController tests', () => {
   test('GET /all-images should get route to retrieve all images', async () => {
     // Pull 'hello-world' image into image_test container
-    execSync(`docker exec -i image_test sh -c "docker pull hello-world "`, {
+    execSync(`docker exec -i image_test sh -c "docker pull hello-world"`, {
       stdio: 'pipe',
     });
     // console.log('TEST1', test1.toString());
@@ -58,6 +69,153 @@ describe('imageController tests', () => {
     expect(dindContainers).toHaveLength(2);
     expect(
       dindContainers.some((container) => container.name === 'hungry_khorana')
+    ).toBeFalsy();
+    expect(Array.isArray(dindContainers)).toBeTruthy();
+    expect(dindContainers.length).toBeGreaterThan(0);
+    expect(typeof dindContainers[0]).toBe('object');
+  });
+
+  test('GET /run-images should run a single container from an image', async () => {
+    execSync(
+      `docker exec -i image_test sh -c "docker pull hello-world; docker pull nginx"`,
+      {
+        stdio: 'pipe',
+      }
+    );
+
+    const dindContainersResponse = execSync(
+      `docker exec -i image_test sh -c "${cmdRunContainerFromImage} ${setRmOption(
+        'yes'
+      )} ${setVolOption(
+        'TEST1',
+        '/App/user'
+      )} -p 9999:80 ${cmdRunContainerFromImageName} ${'TESTCONTAINER'} ${'nginx'}"`,
+      {
+        stdio: 'pipe',
+      }
+    );
+    const dindContainers = parseOutputrunContainerFromImage(
+      dindContainersResponse
+    );
+    console.log('dindContainers:', dindContainers);
+    const dindContainersList = parseOutputContainers(
+      execSync(
+        `docker exec -i image_test sh -c "docker ps -a --format '{{json .}}'"`,
+        {
+          stdio: 'pipe',
+        }
+      )
+    );
+
+    // console.log('running containers', dindContainersList);
+
+    expect(dindContainers.some((container) => container.message)).toBeTruthy();
+    expect(dindContainers).toHaveLength(1);
+    expect(Array.isArray(dindContainers)).toBeTruthy();
+    expect(dindContainers.length).toBeGreaterThan(0);
+    expect(typeof dindContainers[0]).toBe('object');
+
+    expect(
+      dindContainersList.some((container) => container.Image === 'nginx')
+    ).toBeTruthy();
+    expect(
+      dindContainersList.some((container) => container.LocalVolumes === '1')
+    ).toBeTruthy();
+    expect(
+      dindContainersList.some((container) => container.Mounts === 'TEST1')
+    ).toBeTruthy();
+    expect(
+      dindContainersList.some(
+        (container) => container.Names === 'TESTCONTAINER'
+      )
+    ).toBeTruthy();
+    expect(
+      dindContainersList.some(
+        (container) => container.Ports === '0.0.0.0:9999->80/tcp'
+      )
+    ).toBeTruthy();
+    expect(dindContainersList).toHaveLength(3);
+    expect(
+      dindContainersList.some(
+        (container) => container.Names === 'hungry_khorana'
+      )
+    ).toBeFalsy();
+    expect(Array.isArray(dindContainersList)).toBeTruthy();
+    expect(dindContainersList.length).toBeGreaterThan(0);
+    expect(typeof dindContainersList[0]).toBe('object');
+  });
+
+  test('DELETE /prune-all-unused should delete all unused images', async () => {
+    // Pull 'hello-world' image into image_test container
+    execSync(`docker exec -i image_test sh -c "docker pull hello-world"`, {
+      stdio: 'pipe',
+    });
+    // console.log('TEST1', test1.toString());
+    // Get the list of running containers
+    const dindContainersResponse = execSync(
+      `docker exec -i image_test sh -c "${cmdPruneUnusedImages}"`,
+      {
+        stdio: 'pipe',
+      }
+    );
+    // const dindContainersResponse = parseOutputGetAllImages(dindContainers);
+
+    const dindContainers = parseOutputPruneUnusedImages(dindContainersResponse);
+    console.log('dindContainers', dindContainers);
+
+    expect(
+      dindContainers.some((container) => container['Deleted Images:'])
+    ).toBeTruthy();
+    expect(
+      dindContainers.some((container) =>
+        container['Deleted Images:'].includes('untagged: hello-world:latest')
+      )
+    ).toBeTruthy();
+    expect(
+      dindContainers.some((container) => container['Total reclaimed space:'])
+    ).toBeTruthy();
+    expect(dindContainers).toHaveLength(1);
+    expect(
+      dindContainers.some((container) =>
+        container['Deleted Images:'].includes('untagged: mongo:latest')
+      )
+    ).toBeFalsy();
+    expect(Array.isArray(dindContainers)).toBeTruthy();
+    expect(dindContainers.length).toBeGreaterThan(0);
+    expect(typeof dindContainers[0]).toBe('object');
+  });
+
+  test('DELETE /remove-image-by-name should remove image by name', async () => {
+    // Pull 'hello-world' image into image_test container
+    execSync(`docker exec -i image_test sh -c "docker pull hello-world"`, {
+      stdio: 'pipe',
+    });
+    // console.log('TEST1', test1.toString());
+    // Get the list of running containers
+    const dindContainersResponse = execSync(
+      `docker exec -i image_test sh -c "${cmdRemoveSingleImage} hello-world"`,
+      {
+        stdio: 'pipe',
+      }
+    );
+    // const dindContainersResponse = parseOutputGetAllImages(dindContainers);
+
+    const dindContainers = parseOutputRemoveSingleImage(dindContainersResponse);
+    console.log('dindContainers', dindContainers);
+
+    expect(
+      dindContainers.some((container) => container['Deleted'])
+    ).toBeTruthy();
+    expect(
+      dindContainers.some((container) =>
+        container.Deleted.includes('Untagged: hello-world:latest')
+      )
+    ).toBeTruthy();
+    expect(dindContainers).toHaveLength(1);
+    expect(
+      dindContainers.some((container) =>
+        container.Deleted.includes('Untagged: mongo:latest')
+      )
     ).toBeFalsy();
     expect(Array.isArray(dindContainers)).toBeTruthy();
     expect(dindContainers.length).toBeGreaterThan(0);
--- server/controllers/imageController.ts
+++ server/controllers/imageController.ts
@@ -5,6 +5,20 @@ import { promisify } from 'node:util';
 const promisifyExec = promisify(exec);
 
 export const cmdGetAllImages: string = `docker images --format json`;
+export const cmdRunContainerFromImageName: string = `--name `;
+export const cmdRunContainerFromImage: string = `docker run -d`;
+export const cmdPruneUnusedImages: string = `docker image prune -a --force`;
+export const cmdRemoveSingleImage: string = `docker image rm `;
+
+export function parseOutputContainers(data: string | Buffer) {
+  const parsedOutput = data
+    .toString()
+    .trim()
+    .split('\n')
+    .map((item) => JSON.parse(item, undefined));
+
+  return parsedOutput;
+}
 
 export function parseOutputGetAllImages(data: string | Buffer) {
   const parsedOutput = data
@@ -24,22 +38,89 @@ export function setRmOption(remove: string) {
   } else {
     rm = '';
   }
-
   return rm;
 }
 
-// export function setVolOption(volumeName: string) {
-//   if (volumeName && volumeName.trim() !== '') {
-//     // check if fileDirectory is provided
-//     if (fileDirectory && fileDirectory.trim() !== '') {
-//       vol = `-v ${volumeName}:${fileDirectory}`;
-//     } else {
-//       vol = `-v ${volumeName}:/App`;
-//     }
-//   } else {
-//     vol = '';
-//   }
-// }
+export function setVolOption(volumeName: string, fileDirectory: string) {
+  let vol;
+  if (volumeName && volumeName.trim() !== '') {
+    // check if fileDirectory is provided
+    if (fileDirectory && fileDirectory.trim() !== '') {
+      vol = `-v ${volumeName}:${fileDirectory}`;
+    } else {
+      vol = `-v ${volumeName}:/App`;
+    }
+  } else {
+    vol = '';
+  }
+  return vol;
+}
+
+export async function getPortMapping(port: string, image: string) {
+  let portNum = '';
+
+  if (port && port.trim() !== '') {
+    const { stdout, stderr } = await promisifyExec(
+      `docker inspect --format='{{.Config.ExposedPorts}}' ${image}`
+    );
+
+    if (stderr) {
+      throw new Error(
+        'Error in imageController.runContainerFromImage PORT exec call'
+      );
+    }
+
+    const regex = /\[(\d+)\//;
+    const match = stdout.match(regex);
+    const result = match ? match[1] : null;
+    if (result === null) {
+      portNum = ``;
+    } else {
+      portNum = `-p ${port}:${result}`;
+    }
+  }
+
+  return portNum;
+}
+
+export function parseOutputrunContainerFromImage(stdout: string | Buffer) {
+  const output = stdout.toString().trim();
+  return [{ message: output }];
+}
+
+export function parseOutputPruneUnusedImages(stdout: string | Buffer) {
+  const data = stdout.toString().trim();
+  const dataArray = data.split('\n');
+  const deletedImagesIndex = dataArray.findIndex(
+    (item) => item === 'Deleted Images:'
+  );
+  const reclaimedSpaceIndex = dataArray.findIndex((item) =>
+    item.startsWith('Total reclaimed space:')
+  );
+
+  const deletedImages = dataArray
+    .slice(deletedImagesIndex + 1, reclaimedSpaceIndex)
+    .map((item) => item.trim())
+    .filter((item) => item !== ''); // Filter out empty strings
+
+  const reclaimedSpace = dataArray
+    .slice(reclaimedSpaceIndex)
+    .map((item) => item.trim());
+
+  const output = [
+    {
+      'Deleted Images:': deletedImages,
+      'Total reclaimed space:': reclaimedSpace,
+    },
+  ];
+
+  return output;
+}
+
+export function parseOutputRemoveSingleImage(stdout: string | Buffer) {
+  const dataArray = stdout.toString().trim().split('\n');
+  return [{ Deleted: dataArray }];
+}
 
 export const imageController: ImageController = {
   getAllImages: async (
@@ -76,8 +157,7 @@ export const imageController: ImageController = {
   ): Promise<void> => {
     const { name, image, remove, volumeName, fileDirectory, port } = req.body;
 
-    let vol;
-    let portNum;
+    // let portNum;
 
     // // check if name and image is provided
     // if (!name || !image || name.trim() === '' || image.trim() === '') {
@@ -89,7 +169,8 @@ export const imageController: ImageController = {
     //   return next(errorDetails);
     // }
     // let rm;
-    // // check if remove is provided
+    // let vol;
+    // check if remove is provided
     // if (remove === 'yes') {
     //   rm = '--rm';
     // } else {
@@ -97,44 +178,52 @@ export const imageController: ImageController = {
     // }
 
     // check if volume is provided
-    if (volumeName && volumeName.trim() !== '') {
-      // check if fileDirectory is provided
-      if (fileDirectory && fileDirectory.trim() !== '') {
-        vol = `-v ${volumeName}:${fileDirectory}`;
-      } else {
-        vol = `-v ${volumeName}:/App`;
-      }
-    } else {
-      vol = '';
-    }
+    // if (volumeName && volumeName.trim() !== '') {
+    //   // check if fileDirectory is provided
+    //   if (fileDirectory && fileDirectory.trim() !== '') {
+    //     vol = `-v ${volumeName}:${fileDirectory}`;
+    //   } else {
+    //     vol = `-v ${volumeName}:/App`;
+    //   }
+    // } else {
+    //   vol = '';
+    // }
 
     // check if port is provided
-    if (port && port.trim() !== '') {
-      try {
-        const { stdout } = await promisifyExec(
-          `docker inspect --format='{{.Config.ExposedPorts}}' ${image}`
-        );
-        const regex = /\[(\d+)\//;
-        const match = stdout.match(regex);
-        const result = match ? match[1] : null;
-        portNum = `-p ${port}:${result}`;
-      } catch (error) {
-        const errorDetails: ErrorDetails = {
-          log: 'error in imageController.runContainerFromImage PORT catch',
-          err: error,
-          message: `error in exec of imageController.runContainerFromImage PORT catch`,
-        };
-        next(errorDetails);
-      }
-    } else {
-      portNum = '';
-    }
+    // if (port && port.trim() !== '') {
+    //   try {
+    //     const { stdout } = await promisifyExec(
+    //       `docker inspect --format='{{.Config.ExposedPorts}}' ${image}`
+    //     );
+    //     const regex = /\[(\d+)\//;
+    //     const match = stdout.match(regex);
+    //     const result = match ? match[1] : null;
+    //     portNum = `-p ${port}:${result}`;
+    //   } catch (error) {
+    //     const errorDetails: ErrorDetails = {
+    //       log: 'error in imageController.runContainerFromImage PORT catch',
+    //       err: error,
+    //       message: `error in exec of imageController.runContainerFromImage PORT catch`,
+    //     };
+    //     next(errorDetails);
+    //   }
+    // } else {
+    //   portNum = '';
+    // }
 
     try {
+      const portMapping = await getPortMapping(port, image); // Await the result of getPortMapping
+      console.log(
+        `${cmdRunContainerFromImage} ${setRmOption(remove)} ${setVolOption(
+          volumeName,
+          fileDirectory
+        )} ${portMapping} ${cmdRunContainerFromImageName} ${name} ${image}`
+      );
       const { stdout, stderr } = await promisifyExec(
-        `docker run -d ${setRmOption(
-          remove
-        )} ${vol} ${portNum} --name ${name} ${image}`
+        `${cmdRunContainerFromImage} ${setRmOption(remove)} ${setVolOption(
+          volumeName,
+          fileDirectory
+        )} ${portMapping} ${cmdRunContainerFromImageName} ${name} ${image}`
       );
       if (stderr) {
         const errorDetails: ErrorDetails = {
@@ -144,8 +233,7 @@ export const imageController: ImageController = {
         };
         next(errorDetails);
       }
-      const output = stdout.trim();
-      res.locals.ranContainer = [{ message: output }];
+      res.locals.ranContainer = parseOutputrunContainerFromImage(stdout);
       next();
     } catch (error) {
       const errorDetails: ErrorDetails = {
@@ -294,9 +382,7 @@ export const imageController: ImageController = {
     next: NextFunction
   ): Promise<void> => {
     try {
-      const { stdout, stderr } = await promisifyExec(
-        'docker image prune -a --force'
-      );
+      const { stdout, stderr } = await promisifyExec(cmdPruneUnusedImages);
       if (stderr) {
         const errorDetails: ErrorDetails = {
           log: 'error in the exec of imageController.pruneUnusedImages',
@@ -305,31 +391,31 @@ export const imageController: ImageController = {
         };
         next(errorDetails);
       }
-      const dataArray = stdout.trim().split('\n');
-      const deletedImagesIndex = dataArray.findIndex(
-        (item) => item === 'Deleted Images:'
-      );
-      const reclaimedSpaceIndex = dataArray.findIndex((item) =>
-        item.startsWith('Total reclaimed space:')
-      );
+      // const dataArray = stdout.trim().split('\n');
+      // const deletedImagesIndex = dataArray.findIndex(
+      //   (item) => item === 'Deleted Images:'
+      // );
+      // const reclaimedSpaceIndex = dataArray.findIndex((item) =>
+      //   item.startsWith('Total reclaimed space:')
+      // );
 
-      const deletedImages = dataArray
-        .slice(deletedImagesIndex + 1, reclaimedSpaceIndex)
-        .map((item) => item.trim())
-        .filter((item) => item !== ''); // Filter out empty strings
+      // const deletedImages = dataArray
+      //   .slice(deletedImagesIndex + 1, reclaimedSpaceIndex)
+      //   .map((item) => item.trim())
+      //   .filter((item) => item !== ''); // Filter out empty strings
 
-      const reclaimedSpace = dataArray
-        .slice(reclaimedSpaceIndex)
-        .map((item) => item.trim());
+      // const reclaimedSpace = dataArray
+      //   .slice(reclaimedSpaceIndex)
+      //   .map((item) => item.trim());
 
-      const output = [
-        {
-          'Deleted Images:': deletedImages,
-          'Total reclaimed space:': reclaimedSpace,
-        },
-      ];
+      // const output = [
+      //   {
+      //     'Deleted Images:': deletedImages,
+      //     'Total reclaimed space:': reclaimedSpace,
+      //   },
+      // ];
 
-      res.locals.output = output;
+      res.locals.output = parseOutputPruneUnusedImages(stdout);
       next();
     } catch (error) {
       const errorDetails: ErrorDetails = {
@@ -341,59 +427,59 @@ export const imageController: ImageController = {
     }
   },
 
-  //prune only dangling images (ones without a tag)
-  pruneDanglingImages: async (
-    _req: Request,
-    res: Response,
-    next: NextFunction
-  ): Promise<void> => {
-    try {
-      const { stdout, stderr } = await promisifyExec(
-        'docker image prune --force'
-      );
-      if (stderr) {
-        const errorDetails: ErrorDetails = {
-          log: 'error in the exec of imageController.pruneDanglingImages',
-          err: stderr,
-          message: 'error in the exec of imageController.pruneDanglingImages',
-        };
-        next(errorDetails);
-      }
-      const dataArray = stdout.trim().split('\n');
-      const deletedImagesIndex = dataArray.findIndex(
-        (item) => item === 'Deleted Images:'
-      );
-      const reclaimedSpaceIndex = dataArray.findIndex((item) =>
-        item.startsWith('Total reclaimed space:')
-      );
+  // //prune only dangling images (ones without a tag)
+  // pruneDanglingImages: async (
+  //   _req: Request,
+  //   res: Response,
+  //   next: NextFunction
+  // ): Promise<void> => {
+  //   try {
+  //     const { stdout, stderr } = await promisifyExec(
+  //       'docker image prune --force'
+  //     );
+  //     if (stderr) {
+  //       const errorDetails: ErrorDetails = {
+  //         log: 'error in the exec of imageController.pruneDanglingImages',
+  //         err: stderr,
+  //         message: 'error in the exec of imageController.pruneDanglingImages',
+  //       };
+  //       next(errorDetails);
+  //     }
+  //     const dataArray = stdout.trim().split('\n');
+  //     const deletedImagesIndex = dataArray.findIndex(
+  //       (item) => item === 'Deleted Images:'
+  //     );
+  //     const reclaimedSpaceIndex = dataArray.findIndex((item) =>
+  //       item.startsWith('Total reclaimed space:')
+  //     );
 
-      const deletedImages = dataArray
-        .slice(deletedImagesIndex + 1, reclaimedSpaceIndex)
-        .map((item) => item.trim())
-        .filter((item) => item !== ''); // Filter out empty strings
+  //     const deletedImages = dataArray
+  //       .slice(deletedImagesIndex + 1, reclaimedSpaceIndex)
+  //       .map((item) => item.trim())
+  //       .filter((item) => item !== ''); // Filter out empty strings
 
-      const reclaimedSpace = dataArray
-        .slice(reclaimedSpaceIndex)
-        .map((item) => item.trim());
+  //     const reclaimedSpace = dataArray
+  //       .slice(reclaimedSpaceIndex)
+  //       .map((item) => item.trim());
 
-      const output = [
-        {
-          'Deleted Images:': deletedImages,
-          'Total reclaimed space:': reclaimedSpace,
-        },
-      ];
+  //     const output = [
+  //       {
+  //         'Deleted Images:': deletedImages,
+  //         'Total reclaimed space:': reclaimedSpace,
+  //       },
+  //     ];
 
-      res.locals.output = output;
-      next();
-    } catch (error) {
-      const errorDetails: ErrorDetails = {
-        log: 'error in the imageController.pruneDanglingImages catch',
-        err: error,
-        message: 'error in the imageController.pruneDanglingImages catch',
-      };
-      next(errorDetails);
-    }
-  },
+  //     res.locals.output = output;
+  //     next();
+  //   } catch (error) {
+  //     const errorDetails: ErrorDetails = {
+  //       log: 'error in the imageController.pruneDanglingImages catch',
+  //       err: error,
+  //       message: 'error in the imageController.pruneDanglingImages catch',
+  //     };
+  //     next(errorDetails);
+  //   }
+  // },
 
   //remove single image
 
@@ -404,7 +490,9 @@ export const imageController: ImageController = {
   ): Promise<void> => {
     const { id } = req.body;
     try {
-      const { stdout, stderr } = await promisifyExec(`docker image rm ${id}`);
+      const { stdout, stderr } = await promisifyExec(
+        `${cmdRemoveSingleImage} ${id}`
+      );
       if (stderr) {
         const errorDetails: ErrorDetails = {
           log: `error in the imageController.removeSingleImage exec for ${id}`,
@@ -413,9 +501,7 @@ export const imageController: ImageController = {
         };
         next(errorDetails);
       }
-      const dataArray = stdout.trim().split('\n');
-      // .map((item) => JSON.parse(item, undefined));
-      res.locals.output = [{ Deleted: dataArray }];
+      res.locals.output = parseOutputRemoveSingleImage(stdout);
       next();
     } catch (error) {
       const errorDetails: ErrorDetails = {
